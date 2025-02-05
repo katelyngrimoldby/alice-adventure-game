0d68947c52b69eecfedc16bc45ce93df2e525d9a
katelyngrimoldby <katelyngrimoldby@gmail.com>
2025-02-04 17:45:47 -0700

    Write utility functions

---
+++ utility.cpp
@@ -0,0 +1,74 @@
+#include "utility.h"
+
+namespace util {
+  // Non-Pointer functions
+
+  template <typename T>
+  int find_index (std::vector<T> v, T element) {
+    for(int i = 0; i < v.size(); i++) {
+      if(v.at(i) == element) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  template <typename T>
+  bool has_element (std::vector<T> v, T element) {
+    for(int i = 0; i < v.size(); i++) {
+      if(v.at(i) == element) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  // Code from roach's answer on StackOverflow (https://stackoverflow.com/a/65075284)
+  std::vector<std::string> split(std::string text, char delim) {
+    std::string line;
+    std::vector<std::string> v;
+    std::stringstream ss(text);
+
+    while(getline(ss, line, delim)) {
+        v.push_back(line);
+    }
+    return v;
+  }
+
+  // Pointer functions
+
+  template <typename T>
+  int find_index (std::vector<T*>* v, T* element) {
+    for(int i = 0; i < v->size(); i++) {
+      if(v->at(i) == element) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+
+  template <typename T>
+  bool has_element (std::vector<T*>* v, T* element) {
+    for(int i = 0; i < v->size(); i++) {
+      if(v->at(i) == element) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  template <typename T>
+  auto get_instance (std::vector<T>* v, int n) {
+    for(int i = 0; i < v->size(); i++) {
+      if(v->at(i)->getNumber() == n) {      
+        return v->at(i);
+      }
+    }
+
+    return NULL;
+  }
+}
---
+++ utility.h
@@ -0,0 +1,37 @@
+/*
+ utility.h
+ Date: February 4, 2025
+ Author: Katelyn Grimoldby
+*/
+
+#ifndef Utility
+#define Utility
+
+#include <string>
+#include <sstream>
+#include <vector>
+
+namespace util {
+  // Non-pointer functions
+  template <typename T>
+  int find_index (std::vector<T> v, T element);
+
+  template <typename T>
+  bool has_element (std::vector<T> v, T element);
+
+  std::vector<std::string> split(std::string text, char delim);
+
+  // Pointer functions
+  template <typename T>
+  int find_index (std::vector<T*>* v, T* element);
+
+  template <typename T>
+  bool has_element (std::vector<T*>* v, T* element);
+
+  template <typename T>
+  auto get_instance (std::vector<T>* v, int n);
+}
+
+
+
+#endif
