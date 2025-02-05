5f7059e7945872bd1bb9dcc2427ff536984c9387
katelyngrimoldby <katelyngrimoldby@gmail.com>
2025-02-04 18:17:36 -0700

    Write parser for vocabulary

---
+++ vocabulary_parser.cpp
@@ -0,0 +1,100 @@
+#include "vocabulary_parser.h"
+
+namespace parser {
+  Vocabulary::Vocabulary (std::string file, std::vector<ds::Motion*>* motion_v) {
+    actions = new std::vector<ds::Action*>();
+    characters = new std::vector<ds::Character*>();
+    items = new std::vector<ds::Item*>();
+    motions = motion_v;
+    words = new std::vector<ds::Word*>();
+
+    load_file(file);
+  }
+
+  void Vocabulary::parse_actions (void) {
+    if(is_not_current(current_action, current_id - 200)) {
+      current_action = new ds::Action(current_id - 200);
+
+      actions->emplace_back(current_action);
+    }
+
+    current_action->add_word(line_vect.at(1));
+  }
+
+  void Vocabulary::parse_characters (void) {
+    //  Characters only have one vocabulary word each, so is_not_current is not necessary
+    current_character = new ds::Character(current_id - 300);
+
+    characters->emplace_back(current_character);
+
+    current_character->get_vocabulary()->add_word(line_vect.at(1));
+  }
+
+  void Vocabulary::parse_items (void) {
+    //  Items only have one vocabulary word each, so is_not_current is not necessary
+    current_item = new ds::Item(current_id - 100);
+
+    items->emplace_back(current_item);
+
+    current_item->add_word(line_vect.at(1));
+  }
+
+  void Vocabulary::parse_motions (void) {
+    current_motion = util::get_instance(motions, current_id);
+
+    if(current_motion == NULL) {
+      current_motion = new ds::Motion(current_id);
+
+      motions->emplace_back(current_motion);
+    }
+
+    current_motion->add_word(line_vect.at(1));
+  }
+
+  void Vocabulary::parse_words (void) {
+    if(is_not_current(current_word, current_id)) {
+      current_word = new ds::Word(current_id);
+
+      words->emplace_back(current_word);
+    }
+
+    current_word->add_word(line_vect.at(1));
+  }
+
+  void Vocabulary::parse_data (void) {
+    if (current_section == 1) {
+      switch (current_id / 100) {
+        case 0:  //  Motions
+          parse_motions();
+          break;
+        case 1: //  Items
+          parse_items();
+          break;
+        case 2:  //  Actions
+          parse_actions();
+          break;
+        case 3:  //  Characters
+          parse_characters();
+          break;
+        case 4:  //  Other words
+          parse_words();
+      }
+    } else reset_currents();
+  }
+
+  std::vector<ds::Action*>* Vocabulary::return_actions (void) {
+    return actions;
+  }
+
+  std::vector<ds::Character*>* Vocabulary::return_characters (void) {
+    return characters;
+  }
+
+  std::vector<ds::Item*>* Vocabulary::return_items (void) {
+    return items;
+  }
+
+  std::vector<ds::Word*>* Vocabulary::return_words (void) {
+    return words;
+  }
+}
---
+++ vocabulary_parser.h
@@ -0,0 +1,53 @@
+#ifndef Vocabulary_Parser
+#define Vocabulary_Parser
+
+#include <vector>
+#include <string>
+#include "parser.h"
+#include "action.h"
+#include "character.h"
+#include "item.h"
+#include "motion.h"
+#include "word.h"
+#include "utility.h"
+
+namespace parser {
+  class Vocabulary : public Parser {
+    std::vector<ds::Action*>* actions;
+    std::vector<ds::Character*>* characters;
+    std::vector<ds::Item*>* items;
+    std::vector<ds::Motion*>* motions;
+    std::vector<ds::Word*>* words;
+
+    ds::Action* current_action;
+    ds::Character* current_character;
+    ds::Item* current_item;
+    ds::Motion* current_motion;
+    ds::Word* current_word;
+
+    public:
+      Vocabulary (std::string file, std::vector<ds::Motion*>* motion_v);
+
+      void parse_actions (void);
+
+      void parse_characters (void);
+
+      void parse_items (void);
+
+      void parse_motions (void);
+
+      void parse_words (void);
+
+      void parse_data (void);
+
+      std::vector<ds::Action*>* return_actions (void);
+
+      std::vector<ds::Character*>* return_characters (void);
+
+      std::vector<ds::Item*>* return_items (void);
+
+      std::vector<ds::Word*>* return_words (void);
+    };  
+}
+
+#endif
